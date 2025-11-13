fb8216128a583f9868b1b9809f396132fa4dc085
huyth <huyth@HUYNE>
2025-11-13 22:58:47 +0700

    Add project files.

---
+++ GiaoDien.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GiaoDienDangNhap
+{
+    public partial class GiaoDien : Form
+    {
+        public GiaoDien()
+        {
+            InitializeComponent();
+        }
+
+        private void GiaoDien_Load(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Maximized;
+        }
+
+        private void panel_NoiDung_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Xóa các control cũ trong panel (nếu có)
+            panel_NoiDung.Controls.Clear();
+
+            // Tạo instance của form ThuCungGiaoDien
+            ThuCungGiaoDien formThuCung = new ThuCungGiaoDien();
+
+            // Set các thuộc tính để form hiển thị như một control
+            formThuCung.TopLevel = false;
+            formThuCung.FormBorderStyle = FormBorderStyle.None;
+            formThuCung.Dock = DockStyle.Fill;
+
+            // Thêm form vào panel
+            panel_NoiDung.Controls.Add(formThuCung);
+
+            // Hiển thị form
+            formThuCung.Show();
+        }
+
+
+    }
+}
